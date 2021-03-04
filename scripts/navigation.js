04900da74744b78a1f09358ea3cc2db4719eeed8
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-04 19:57:03 +0100

    construct navigation script for changing titles.

---
+++ scripts/navigation.js
@@ -0,0 +1,52 @@
+﻿const navigationElements = document.querySelectorAll("#brdmenu > ul > li");
+console.log(navigationElements);
+
+navigationElements.forEach((element) => {
+    let link = element.querySelector("a");
+    const navPrefix = "nav";
+
+    switch (element.id) {
+        case `${navPrefix}index`:
+            link.textContent = "Miasto";
+            break;
+
+        case `${navPrefix}forums`:
+            link.textContent = "Miasto";
+
+        case `${navPrefix}userlist`:
+            link.textContent = "Obywatele";
+            break;
+
+        case `${navPrefix}rules`:
+            link.textContent = "Prawo";
+            break;
+
+        case `${navPrefix}search`:
+            link.textContent = "Szukaj";
+            break;
+
+        case `${navPrefix}admin`:
+            link.textContent = "Władza";
+            link.style.color = "red";
+            break;
+
+        case `${navPrefix}pm`:
+            link.textContent = "Wiadomości";
+            break;
+
+        case `${navPrefix}login`:
+            link.textContent = "Zamelduj";
+            break;
+
+        case `${navPrefix}logout`:
+            link.textContent = "Wymelduj";
+            break;
+
+        case `${navPrefix}profile`:
+            link.textContent = "Wizerunek";
+            break;
+
+        default:
+            break;
+    }
+});
