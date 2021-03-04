5652f7480de9eab0fd1dfa61706f0f9bf2e6756f
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-04 21:32:57 +0100

    import own stylesheet version in place of Mercury.

---
+++ scripts/importStylesheet.js
@@ -0,0 +1,8 @@
+const styleSheet = document.querySelector('link[href="style/Mercury.css"]');
+
+console.log(styleSheet);
+
+styleSheet.setAttribute(
+    "href",
+    "https://egildia.pl/forum/styles/mercury/Mercury.css"
+);
