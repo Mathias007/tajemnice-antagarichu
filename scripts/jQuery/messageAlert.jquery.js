8b14f174430db6d50cfec2455e53df3c0407e235
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-05 23:21:16 +0100

    add messageAlert built in vanilla js; replace unused jQuery scripts.

---
+++ scripts/jQuery/messageAlert.jquery.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function () {
+    jQuery("#punforums.pun li.pmlink").val(function () {
+        alert(
+            "Uwaga! Przybyła nowa wiadomość na twoją prywatną skrzynkę. Koniecznie przejrzyj pocztę!"
+        );
+    });
+    jQuery("#punindex.pun li.pmlink").val(function () {
+        alert(
+            "Uwaga! Przybyła nowa wiadomość na twoją prywatną skrzynkę. Koniecznie przejrzyj pocztę!"
+        );
+    });
+});
--- scripts/messageAlert.js
+++ scripts/messageAlert.js
@@ -1,12 +1,9 @@
-﻿jQuery(document).ready(function () {
-    jQuery("#punforums.pun li.pmlink").val(function () {
-        alert(
-            "Uwaga! Przybyła nowa wiadomość na twoją prywatną skrzynkę. Koniecznie przejrzyj pocztę!"
-        );
-    });
-    jQuery("#punindex.pun li.pmlink").val(function () {
-        alert(
-            "Uwaga! Przybyła nowa wiadomość na twoją prywatną skrzynkę. Koniecznie przejrzyj pocztę!"
-        );
-    });
-});
+﻿const newMessageLink =
+    document.querySelector("#punforums.pun li.pmlink") ||
+    document.querySelector("#punindex.pun li.pmlink");
+
+if (newMessageLink) {
+    alert(
+        "Uwaga! Przybyła nowa wiadomość na twoją prywatną skrzynkę. Koniecznie przejrzyj pocztę!"
+    );
+}
