dc4501345216416273225413160e200f620f0ee8
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-06 22:03:43 +0100

    Add classess for advertisement box.

--- scripts/importStylesheet.js
+++ scripts/importStylesheet.js
@@ -1,3 +1,4 @@
+// Change default Mercury stylesheet on the own one
 const styleSheet = document.querySelector('link[href="style/Mercury.css"]');
 
 console.log(styleSheet);
@@ -6,3 +7,14 @@ styleSheet.setAttribute(
     "href",
     "https://egildia.pl/forum/styles/mercury/Mercury.css"
 );
+
+// Add classess for the advertisement box
+const advEl = document.querySelector("#announce + div");
+console.log(advEl);
+
+advEl.removeAttribute('style');
+advEl.classList.add("block", "advertisement");
+
+// Remove adv class from admin panel
+const adminConsole = document.getElementById("adminconsole");
+adminConsole.classList.remove("advertisement");
