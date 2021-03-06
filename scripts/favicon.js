f0ca56382c2f7a16f8e05aa283ebabaffda77be1
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-06 16:57:06 +0100

    generate link elements for favicons in head dynamically.

---
+++ scripts/favicon.js
@@ -0,0 +1,45 @@
+{
+    /* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest"></link> */
+}
+
+const faviconData = {
+    appleTouch: {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+    },
+    icon32: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+    },
+    icon16: {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+    },
+    manifest: {
+        rel: "manifest",
+        href: "/site.webmanifest",
+    },
+};
+
+Object.values(faviconData).forEach((linkData) => {
+    const path = "https://egildia.pl/forum/favicon";
+    let order = 0;
+
+    const linkEl = document.createElement("link");
+    linkEl.rel = linkData.rel;
+    linkEl.type = linkData.type;
+    linkEl.sizes = linkData.sizes;
+    linkEl.href = `${path}${linkData.href}`;
+
+    document.getElementsByTagName("head")[order].appendChild(linkEl);
+
+    order++;
+});
